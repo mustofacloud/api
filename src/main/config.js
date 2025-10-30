9f9185b2dee4bef9ffface5480b827d6cfbbd4d8
MasTopa <topaboju@gmail.com>
2025-10-30 13:01:18 +0700

    Update config.js

--- src/main/config.js
+++ src/main/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   discordWebhookUrl: "https://discord.com/api/webhooks/1433263008116768978/U8uldGU0b7MTVfvHcke0uJnmtuJAAKJ3FwNVe7CUVSyvVkKfi8vM07pM2zqqnG4h7mLz",
   botAvatarUrl:
-    ".....",
+    "https://cdn.discordapp.com/avatars/1181228695889911858/b5a01535ec3718c7e483c3d013926a6c.png?size=1024",
   maintenanceMode: false,
   port: 1200,
 };
